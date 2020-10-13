175f0b76f0bacf960faf2d06405751adf605eb13
Wesley <wesleyalkemade@outlook.com>
2020-10-14 00:34:56 +0200

    feat(Command): add new command to create a team

---
+++ src/main/java/com/dirk/commands/server_moderation/CreateTeamCommand.java
@@ -0,0 +1,93 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Wesley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dirk.commands.server_moderation;
+
+import com.dirk.helper.EmbedHelper;
+import com.dirk.models.command.Command;
+import com.dirk.models.command.CommandArgument;
+import com.dirk.models.command.CommandArgumentType;
+import com.dirk.models.command.CommandParameter;
+import org.javacord.api.entity.permission.PermissionType;
+import org.javacord.api.entity.permission.PermissionsBuilder;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CreateTeamCommand extends Command {
+    public CreateTeamCommand() {
+        this.commandName = "createteam";
+        this.description = "Creates a text and voice channel and adds the highlighted users to the team.";
+        this.group = "Server moderation";
+
+        this.requiresAdmin = true;
+        this.guildOnly = true;
+
+        this.commandArguments.add(new CommandArgument("team name", "The name of the team \n`user highlights`: The users you want to add to the team. Tag one or multiple users with `@Username`", CommandArgumentType.String));
+    }
+
+    @Override
+    public void execute(MessageCreateEvent messageCreateEvent) {
+    }
+
+    @Override
+    public void execute(MessageCreateEvent messageCreateEvent, List<CommandParameter> commandParams) {
+        String commandString = (String) commandParams.get(0).getValue();
+        List<String> splittedString = Arrays.stream(commandString.split("(?=<@[0-9!]+>)")).map(String::trim).collect(Collectors.toList());
+        String teamName = splittedString.subList(0, 1).get(0);
+        List<String> highlightedUsers = splittedString.subList(1, splittedString.size());
+
+        Server server = messageCreateEvent.getServer().get();
+
+        server.createRoleBuilder()
+                .setColor(new Color(96, 125, 136))
+                .setName(teamName).create().whenComplete((role, throwable) -> {
+            server.createChannelCategoryBuilder()
+                    .setName(teamName)
+                    .addPermissionOverwrite(server.getEveryoneRole(), new PermissionsBuilder().setDenied(PermissionType.READ_MESSAGES).build())
+                    .addPermissionOverwrite(role, new PermissionsBuilder().setAllowed(PermissionType.READ_MESSAGES).build()).create().whenComplete((channelCategory, throwable1) -> {
+                server.createTextChannelBuilder().setName(teamName).setCategory(channelCategory).create();
+                server.createVoiceChannelBuilder().setName(teamName).setCategory(channelCategory).create();
+            });
+
+            highlightedUsers.forEach(userString -> {
+                userString = userString.replace("<", "")
+                        .replace("@", "")
+                        .replace(">", "")
+                        .replace("!", "");
+
+                server.getMemberById(userString).ifPresent(user1 -> user1.addRole(role));
+            });
+        }).whenComplete((role, throwable) ->
+                messageCreateEvent
+                        .getChannel()
+                        .sendMessage(EmbedHelper.genericSuccessEmbed("Successfully created the role, voice and text channel for `" + teamName + "`! \n\nAccess has been given to " + String.join(", ", highlightedUsers) + ".", messageCreateEvent.getMessageAuthor().getDiscriminatedName())));
+    }
+}
