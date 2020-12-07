e8cdd96b08cc6284d374ad7b021b81514b04f4d2
Wesley <wesleyalkemade@outlook.com>
2020-12-07 16:00:50 +0100

    feat(Tournament): Add tournament setup commands

---
+++ src/main/java/com/dirk/commands/tournament/SetTeamsCommand.java
@@ -0,0 +1,92 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Wesley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dirk.commands.tournament;
+
+import com.dirk.helper.EmbedHelper;
+import com.dirk.helper.TournamentHelper;
+import com.dirk.models.command.Command;
+import com.dirk.models.command.CommandArgument;
+import com.dirk.models.command.CommandArgumentType;
+import com.dirk.models.command.CommandParameter;
+import com.dirk.models.tournament.Tournament;
+import com.dirk.repositories.TournamentRepository;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SetTeamsCommand extends Command {
+    private final TournamentRepository tournamentRepository;
+
+    @Autowired
+    public SetTeamsCommand(TournamentRepository tournamentRepository) {
+        this.commandName = "setteams";
+        this.description = "Set whether or not this tournament will be a 1v1 or a team vs team tournament.";
+        this.group = "Tournament management";
+
+        this.requiresAdmin = true;
+        this.guildOnly = true;
+
+        this.commandArguments.add(new CommandArgument("teams", "Enter `true` for team vs team, enter `false` for 1v1", CommandArgumentType.String));
+
+        this.tournamentRepository = tournamentRepository;
+    }
+
+    @Override
+    public void execute(MessageCreateEvent messageCreateEvent) {
+    }
+
+    @Override
+    public void execute(MessageCreateEvent messageCreateEvent, List<CommandParameter> commandParams) {
+        String isTeamTournament = (String) commandParams.stream().findFirst().get().getValue();
+
+        if (!isTeamTournament.equals("true") && !isTeamTournament.equals("false")) {
+            messageCreateEvent
+                    .getChannel()
+                    .sendMessage(EmbedHelper.genericErrorEmbed(this.getCommandHelpFormat("Invalid `teams` argument given.\n\n"), messageCreateEvent.getMessageAuthor().getDiscriminatedName()));
+            return;
+        }
+
+        Tournament existingTournament = TournamentHelper.getRunningTournament(messageCreateEvent, tournamentRepository);
+
+        if (existingTournament == null) {
+            messageCreateEvent
+                    .getChannel()
+                    .sendMessage(EmbedHelper.genericErrorEmbed("There is no tournament running in this server.", messageCreateEvent.getMessageAuthor().getDiscriminatedName()));
+            return;
+        }
+
+        existingTournament.setIsTeamTournament(Boolean.valueOf(isTeamTournament));
+        tournamentRepository.save(existingTournament);
+
+        String message = Boolean.parseBoolean(isTeamTournament) ? "Set the tournament as a team tournament." : "Set the tournament as a solo tournament.";
+
+        messageCreateEvent
+                .getChannel()
+                .sendMessage(EmbedHelper.genericSuccessEmbed(message, messageCreateEvent.getMessageAuthor().getDiscriminatedName()));
+    }
+}
---
+++ src/main/java/com/dirk/helper/TournamentHelper.java
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Wesley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dirk.helper;
+
+import com.dirk.models.tournament.Tournament;
+import com.dirk.repositories.TournamentRepository;
+import org.javacord.api.entity.permission.Role;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.event.message.MessageCreateEvent;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TournamentHelper {
+    /**
+     * Check if a tournament is running. Sends a message if there isn't a tournament running.
+     *
+     * @param messageCreateEvent   the MessageCreateEvent object
+     * @param tournamentRepository the TournamentRepository
+     * @return Whether or not a tournament is running in the given server
+     */
+    public static Tournament getRunningTournament(MessageCreateEvent messageCreateEvent, TournamentRepository tournamentRepository) {
+        Server server = messageCreateEvent.getServer().get();
+        return tournamentRepository.getTournamentByServerSnowflake(server.getIdAsString());
+    }
+
+    /**
+     * Get a role by the given string and server
+     *
+     * @param role   the Role to look for
+     * @param server the Server to look in
+     * @return A role or null
+     */
+    public static Role getRoleByString(String role, Server server) {
+        Pattern rolePattern = Pattern.compile("<@&([0-9]+)>");
+        Matcher matcher = rolePattern.matcher(role);
+
+        Optional<Role> foundRole;
+
+        // Check if role is typed out or mention
+        if (matcher.find()) {
+            String roleId = matcher.group(1);
+            foundRole = server.getRoleById(roleId);
+        } else {
+            foundRole = server.getRolesByName(role).stream().findFirst();
+        }
+
+        return foundRole.orElse(null);
+    }
+}
